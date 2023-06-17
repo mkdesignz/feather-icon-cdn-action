d24a8791c192fd98b7b86c8c89e9fa22732f0054
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2023-06-17 19:38:29 -0400

    Make the svgs customizable via query strings

--- src/api/icons/icons.controller.ts
+++ src/api/icons/icons.controller.ts
@@ -16,9 +16,21 @@ export class IconsController {
     return res.status(200).send(JSON.parse(JSON.stringify(allIcon)));
   }
   public getSingleIcon(req: Request, res: Response): unknown {
-    const { iconsName} = req.params;
+    const {
+      color,
+      size,
+      fill,
+      stroke_width
+    } = req.query;
+    const { iconsName } = req.params;
     try {
-      const icon = icons[iconsName].toSvg();
+      const icon = icons[iconsName].toSvg({
+        "color": color,
+        "stroke-width": stroke_width || 2,
+        "width": size || 24,
+        "height": size || 24,
+        "fill": fill || "transparent"
+      });
       return res
         .header("Content-Type","image/svg+xml")
         .send(icon);
