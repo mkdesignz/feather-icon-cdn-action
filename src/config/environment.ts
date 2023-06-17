ec83bcd921032dec3cc5103cd3dde2735a3c0126
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2023-06-17 01:52:24 -0400

    Update environment.ts

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import { Server } from './variables';
 import { development } from './enviornment/dev';
 import { production } from './enviornment/prod';
-import { init } from './enviornment/init';
 import helmet from 'helmet';
 
 export const environment = express();
@@ -13,10 +12,6 @@ switch (Server.environment) {
   case 'Development':
   case 'development':
     environment.use(development);
-    break;
-  case 'Init':
-  case 'init':
-    environment.use('/init', init);
     break;
   case 'Production':
   case 'production':
